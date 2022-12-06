074bfa0b8eea9f48002fe0b6ae8fb954f5b4a4d4
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-06 23:56:25 +0530

    forloopAndstring

---
+++ 70-forLoopAndstring-CipherSchools.py
@@ -0,0 +1,11 @@
+# name="Shivam"
+# for i in name:
+#     print(i)
+
+
+# number=input("enter number here: ")
+# total=0
+# for i in range(len(number)):
+#     total+=int(number[i])
+# print(total)    
+
