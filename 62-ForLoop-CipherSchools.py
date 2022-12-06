02f16477f03abe48ba1332a3724e0f979e92c1e2
Shivam@911339 <shivamraj.tiwari787898@gmail.com>
2022-12-06 23:45:48 +0530

    loop

---
+++ 62-ForLoop-CipherSchools.py
@@ -0,0 +1,13 @@
+# sum from  1 to 20
+total=0
+for i in range(1,21):# here is 21 not included in range
+    total+=i
+print(total)
+
+# input from user
+num=int(input("Enter your Number: "))
+total=0
+for i in range(1,num+1):
+    total+=i
+print(total)    
+  
