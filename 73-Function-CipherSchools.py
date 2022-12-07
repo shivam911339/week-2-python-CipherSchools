076cce9a5b1781570bbbfde8f741f29d289a76bd
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-07 23:55:26 +0530

    function

---
+++ 73-Function-CipherSchools.py
@@ -0,0 +1,20 @@
+# define a function which add 2 number
+
+def add_two(num1,num2):
+    return num1+num2
+# total=add_two(10,20) 
+# print(total)   
+
+
+# num1=int(input("Enter your number1: "))
+# num2=int(input("Enter your number2: "))
+# total=add_two(num1,num2)
+# print(total)
+
+# concatinate the string
+def add_string(a,b):
+    return a+b
+a=input()   
+b=input() 
+fullName=add_string(a,b) 
+print(fullName)   
