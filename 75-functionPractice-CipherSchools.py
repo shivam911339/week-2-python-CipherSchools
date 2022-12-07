16508f35b3da147e9d42ba14e17d9dd159885857
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-07 23:56:28 +0530

    function practice

---
+++ 75-functionPractice-CipherSchools.py
@@ -0,0 +1,45 @@
+# function practice
+# char=input("enter a word")
+# def last_charecter(char):
+#     return char[-1]
+# word=last_charecter(char)
+
+# print(word)
+
+
+# number odd or even
+# num=int(input("Enter your number: "))
+# def oddeven_number(num):
+#     if num%2==0:
+#         return "Even number"
+#     else:
+#         return "Odd number"
+# print(oddeven_number(num))            
+
+
+# num = int(input("Enter your number: "))
+
+
+# def oddeven_number(num):
+#     if num % 2 == 0:
+#         return "Even number"
+    
+#     return "Odd number"
+
+
+# print(oddeven_number(num))
+
+
+def is_even(num):
+    if num%2==0:
+        return True
+    return False
+print(is_even(9)  )  
+
+
+
+def is_even(num):
+    return num%2==0
+    True
+print(is_even(10) )   
+ 
