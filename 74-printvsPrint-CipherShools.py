0f3e9649aef5c2c7a4ae9008d388042fcf833db7
Shivam Kumar Tiwari <114800211+shivam911339@users.noreply.github.com>
2022-12-07 23:56:08 +0530

    printvsreturn

---
+++ 74-printvsPrint-CipherShools.py
@@ -0,0 +1,12 @@
+# return vs print
+def add_two(a,b):
+    return a+b
+total=add_two(10,20)
+print(total)
+
+
+def add_two(a, b):
+    print(a+b)
+# total = add_two(10, 20)
+# print(total)
+add_two(10,20)
